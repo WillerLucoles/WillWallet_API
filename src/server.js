9acdf3108774e20bc6910e5f1385228229996bd6
WillerLucoles <willerlob@gmail.com>
2024-10-29 22:30:32 -0300

    Created_Pastes
    Arquitetura:
    Routes
    Controller
    Services
    Respositories
    
    Pastas Criadas com testes de retorno json.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
-import express from "express";
+import express , {json} from "express";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
-app.listen(2400, ()=> console.log("Server listening in port 5000"));
+app.use(json());
+app.use(authRouter);
+
+app.listen(5000, ()=> console.log("Server listening in port 5000"));
