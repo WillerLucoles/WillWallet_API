c9dedad973963e6870b3d2b67f6c2508fe35cc10
WillerLucoles <willerlob@gmail.com>
2024-10-31 23:30:47 -0300

    Bcrypt_authPassword
    Install Bcrypt

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,12 @@
+import bcrypt from "bcrypt";
+import authRepository from "../repositories/authRepository.js";
+
 function signup(body) {
-  return body;
+  const hasPassword = bcrypt.hashSync(body.password, 10);
+
+ return authRepository.create({...body, password: hasPassword})
 }
+
 export default {
   signup,
 };
