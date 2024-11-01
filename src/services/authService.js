fd68d3373cdd46184402d759ba07076197c5be2c
WillerLucoles <willerlob@gmail.com>
2024-10-31 23:49:37 -0300

    Created_NewUser schema
    Ajuste de criação de novos usuários.
    Conexões async com mongoDb Atlas

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,12 @@
 import bcrypt from "bcrypt";
 import authRepository from "../repositories/authRepository.js";
 
-function signup(body) {
+async function signup(body) {
   const hasPassword = bcrypt.hashSync(body.password, 10);
+  const userExist = await authRepository.findByEmail(body.email)
+  if(userExist) throw new Error('User already exist!');
 
- return authRepository.create({...body, password: hasPassword})
+ return await authRepository.create({...body, password: hasPassword})
 }
 
 export default {
