2f8f6dd69557c9fe5f9da5df00af701419e9bc1c
WillerLucoles <willerlob@gmail.com>
2024-11-04 23:49:58 -0300

    Validate_Joi
    Usar Joi - validação de dados.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import authController from "../controllers/authController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { validationSchemaMiddleware } from "../middlewares/validationSchemaMiddleware.js";
+import { createUser } from "../schemas/validation/createUser.js";
+import { authUser } from "../schemas/validation/authUser.js";
 
 const authRouter = Router();
-authRouter.post("/signup", authController.signup);
-authRouter.post("/signin", authController.signin);
+authRouter.post("/signup",validationSchemaMiddleware(createUser), authController.signup);
+authRouter.post("/signin", validationSchemaMiddleware(authUser), authController.signin);
 authRouter.get("/me", authMiddleware, authController.userLogged);
 
 
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import transactionController from "../controllers/transactionController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { validationSchemaMiddleware } from "../middlewares/validationSchemaMiddleware.js";
+import { createTransaction } from "../schemas/validation/createTransaction.js";
 
 const transactionRouter = Router();
 
@@ -8,11 +10,13 @@ transactionRouter.use(authMiddleware)
 
 transactionRouter.post(
     "/transaction", 
+    validationSchemaMiddleware(createTransaction),
     transactionController.create
 );
 
 transactionRouter.get(
     '/transaction',
+    
     transactionController.findAllByUser
 );
 
